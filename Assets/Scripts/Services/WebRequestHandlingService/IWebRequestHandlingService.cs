d9acb9a7627cabf5dcfb09c065d776ebe015c565
vadimgaiseniuk <88740661+vadimgaiseniuk@users.noreply.github.com>
2025-10-24 21:07:53 +0300

    [+] Web Request Handling Service

---
+++ Assets/Scripts/Services/WebRequestHandlingService/IWebRequestHandlingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Services.WebRequestHandlingService
+{
+    public interface IWebRequestHandlingService
+    {
+        Task<Sprite> GetImageAsync(string url);
+    }
+}
---
+++ Assets/Scripts/Services/WebRequestHandlingService/WebRequestHandlingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Services.WebRequestHandlingService
+{
+    public class WebRequestHandlingService : IWebRequestHandlingService
+    {
+        public async Task<Sprite> GetImageAsync(string url)
+        {
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+            
+            try
+            {
+                AsyncOperation asyncOperation = request.SendWebRequest();
+
+                if (asyncOperation == null)
+                    return null;
+
+                while (!asyncOperation.isDone)
+                {
+                    await Task.Yield();
+                    Debug.Log(asyncOperation.progress);
+                }
+
+                if (request.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError("Failed to load avatar");
+                    return null;
+                }
+                
+                Texture2D downloadedAvatarTexture = DownloadHandlerTexture.GetContent(request);
+                
+                return Sprite.Create(downloadedAvatarTexture,  new Rect(0, 0, downloadedAvatarTexture.width, downloadedAvatarTexture.height), Vector2.zero);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to load avatar with exception: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
