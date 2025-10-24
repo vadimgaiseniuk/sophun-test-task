782b9d12930f676f0a2a6254cfdf96a48114a088
vadimgaiseniuk <88740661+vadimgaiseniuk@users.noreply.github.com>
2025-10-24 11:44:23 +0300

    [+] Json Handling Service

---
+++ Assets/Scripts/Services/DataSerializationService/IDataSerializationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.DataSerializationService
+{
+    public interface IDataSerializationService
+    {
+        Task<T> DeserializeAsync<T>(string data);
+    }
+}
---
+++ Assets/Scripts/Services/DataSerializationService/IJsonSerializationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Services.DataSerializationService
+{
+    public class JsonDataSerializationService : IDataSerializationService
+    {
+        public async Task<T> DeserializeAsync<T>(string data)
+        {
+            try
+            {
+                T deserializedObject = await Task.Run( () => JsonConvert.DeserializeObject<T>(data));
+                
+                return deserializedObject;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Deserialization of data failed with exception: {e.Message}");
+                return default;
+            }
+        }
+    }
+}
